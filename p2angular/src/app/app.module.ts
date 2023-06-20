94652b7b2ad49c9e410f534fd30dc81588439a42
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-06-20 09:00:00 -0300

    criação dos componentes & edição do visual do site

---
+++ p2angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CadempresasComponent } from './cadempresas/cadempresas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadempresasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ p2angular/src/app/cadempresas/cadempresas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadempresas',
+  templateUrl: './cadempresas.component.html',
+  styleUrls: ['./cadempresas.component.css']
+})
+export class CadempresasComponent {
+
+}
